b09b2ee17c02c2ca79744323fd003ff02aa856ca
Niko D <nikodojan@live.de>
2021-12-07 14:02:29 +0100

    a working version

--- ParkingSeleniumTests/UnitTest1.cs
+++ ParkingSeleniumTests/UnitTest1.cs
@@ -16,17 +16,20 @@ namespace ParkingSeleniumTests
         [TestInitialize]
         public void SetUp()
         {
-            //Location of the web driver
-             _iWebDriver = new ChromeDriver("C:\\WebDriver");
-            //Telling the driver which url to go to
-            _iWebDriver.Navigate().GoToUrl(@"http://localhost:8081/");
+            //Location of the web driver (leave empty if nuget package with driver is installed)
+             _iWebDriver = new ChromeDriver();
+            //Telling the driver which url to go to. Change the url and port according to you local environment (Vue dev server)
+            _iWebDriver.Navigate().GoToUrl(@"http://localhost:8080/");
         }
         [TestMethod]
         public void TestParkingLotOccupied()
         {
             IWebElement iWebElement = _iWebDriver.FindElement(By.Id("1"));
-            var colorBefore = iWebElement.GetAttribute("background");
+            var colorBefore = iWebElement.GetCssValue("background");
             _iWebDriver.ExecuteJavaScript("arguments[0].setAttribute('class', 'slot occupied')", iWebElement);
+            var colorAfter = iWebElement.GetCssValue("background");
+
+            Assert.AreNotEqual(colorAfter, colorBefore);
         }
     }
 }
